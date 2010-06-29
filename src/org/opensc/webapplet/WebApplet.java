02da8ed07beb733a4a6e128415d5a1b2ec1911c1
Martin Paljak <martin@paljak.pri.ee>
2010-06-29 12:14:29 +0300

    Hello

--- src/org/opensc/webapplet/WebApplet.java
+++ src/org/opensc/webapplet/WebApplet.java
@@ -7,16 +7,27 @@ import java.awt.Color;
 import javax.swing.BorderFactory;
 import javax.swing.JApplet;
 import javax.swing.JLabel;
+import netscape.javascript.JSObject;
 
 import javax.smartcardio.*;
 
 public class WebApplet extends JApplet {
+	private JSObject window;
 	public void init() {
-		JLabel label = new JLabel("SignApplet");
+		JLabel label = new JLabel("OpenSC Web Control Applet");
 		label.setHorizontalAlignment(JLabel.CENTER);
 		label.setBorder(BorderFactory.createMatteBorder(1, 1, 2, 2, Color.black));
 		getContentPane().add(label, BorderLayout.CENTER);
 		System.out.println("init");
-
+		try {
+			window = JSObject.getWindow(this);
+		} catch (Exception e) {
+			System.out.println("No HTML container");
+		}
 	}
+	
+	public void sign(String param, String cb1) {
+		System.out.println("sign");
+		window.eval("window.alert(\'helo\');");
+	} 
 }
